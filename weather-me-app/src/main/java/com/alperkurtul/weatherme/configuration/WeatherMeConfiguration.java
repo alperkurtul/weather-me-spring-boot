675c0dbf087e4318b550ef9d21cf327d0d531371
alperkurtul <alperkurtul@gmail.com>
2020-04-01 09:55:07 +0300

    commit and push

--- weather-me-app/src/main/java/com/alperkurtul/weatherme/configuration/WeatherMeConfiguration.java
+++ weather-me-app/src/main/java/com/alperkurtul/weatherme/configuration/WeatherMeConfiguration.java
@@ -1,8 +1,13 @@
 package com.alperkurtul.weatherme.configuration;
 
+import com.alperkurtul.weatherme.data.constant.ApplicationConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WeatherMeConfiguration {
@@ -10,6 +15,13 @@ public class WeatherMeConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.forLanguageTag(ApplicationConstants.DEFAULT_DIALECT));
+        return localeResolver;
     }
 
 }
