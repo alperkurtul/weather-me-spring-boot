b548bfaed6b589d6cd7a259a745500c9473e34bb
alperkurtul <alperkurtul@gmail.com>
2023-01-05 12:24:38 +0300

    commit and push

--- weather-me-data/src/main/java/com/alperkurtul/weatherme/repository/WeatherHistoryRepository.java
+++ weather-me-data/src/main/java/com/alperkurtul/weatherme/repository/WeatherHistoryRepository.java
@@ -5,7 +5,6 @@ import com.alperkurtul.weatherme.model.WeatherHistoryId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-
 import java.time.LocalDateTime;
 import java.util.Optional;
 
